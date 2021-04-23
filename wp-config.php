f74ff4472121c9f4486b5931d6fae9e7772ea572
islandgo <islandgoph@gmail.com>
2021-04-23 09:55:30 +0800

    update

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'isdtemplate.com' );
+define( 'DB_NAME', 'mroller_pending.com' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
