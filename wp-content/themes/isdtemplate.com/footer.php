e1a4b9892e3442399facbab953c4d8ec9521ee6c
petercenir07 <peter.cenir@gmail.com>
2022-06-02 16:12:33 +0800

    footer update

--- wp-content/themes/isdtemplate.com/footer.php
+++ wp-content/themes/isdtemplate.com/footer.php
@@ -8,8 +8,12 @@
 		<footer class="footer">
 			<div class="container">
 				<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'menu_class' => 'footernav', 'theme_location' => 'primary-menu','depth'=>1 ) ); ?>
-				<div class="copyright">
-					&copy; <?php echo date('Y') ?> Agent Image. <!-- aios shortcode here -->
+				<div class="footer-lower">
+					<p class="copyright">Copyright © <?php echo do_shortcode('[currentYear]')?> <span class="sitename"> Lynn Marie Oddo. </span>All rights reserved. <a class="sitemap" href="<?php echo do_shortcode('[blogurl]')?>/sitemap">Sitemap</a> | <?php echo do_shortcode('[agentimage_credits credits="Real Estate Website Design by <a target="_blank" href="https://www.agentimage.com" style="text-decoration:underline;font-weight:bold">Agent Image</a>"]'); ?></p>
+					<div class="mls">
+						<em class="ai-font-eho" title="MLS"></em>	
+						<em class="ai-font-realtor-mls" title="MLS"></em>
+					</div>
 				</div>
 			</div>		
 		</footer>
